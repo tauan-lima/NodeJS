41b87600d4cb04d74de7c97c09ce62076d5e5895
Tauan Lima <contato@tauanlima.com.br>
2025-03-15 19:41:14 -0300

    URL da requisição ROTAS

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-  const {method} = req
-  res.writeHead(201).end('Criado com sucesso!');
+  const {url, method} = req
+  
+  if(method === 'GET' && url === '/products') {
+    res.end("Lista de Produtos")
+  } else if(method === 'POST' && url === '/products') {
+    res.writeHead(201).end("Produto Cadastrado")
+  } else{
+    res.writeHead(404).end("Rota não encontrada")
+  }
 });
 server.listen(3000)
